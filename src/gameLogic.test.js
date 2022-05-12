0af68b35b909766f083be3e30364deb15e0af9f8
ridwan <irfanridwan20@gmail.com>
2022-05-12 22:25:28 +0600

    added tests for receiveAttack function

--- src/gameLogic.test.js
+++ src/gameLogic.test.js
@@ -12,3 +12,18 @@ test('placeShip function add ships to the shipList array',()=>{
     expect(gameboard.shipList.length).toBe(1);
 })
 
+test('receiveAttack function sends the hit function to the correct ship',()=>{
+    const gameboard=gameBoard();
+    gameboard.placeShip('carrier',2)
+    gameboard.receiveAttack(2);
+    expect(gameboard.shipList[0].hitPositions).toContain(2);
+    gameboard.receiveAttack(3);
+    expect(gameboard.shipList[0].hitPositions).toContain(3);
+})
+
+test('gameBoard keep record of the missed attacks',()=>{
+    const gameboard=gameBoard();
+    gameboard.placeShip('carrier',2)
+    gameboard.receiveAttack(1);
+    expect(gameboard.missedHits).toContain(1)
+})
