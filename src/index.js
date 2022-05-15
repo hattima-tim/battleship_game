b54b6a38e3f2713e1e41822353c750a1c83dd1c7
ridwan <irfanridwan20@gmail.com>
2022-05-15 10:27:28 +0600

    added createGameBoardDom function

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import {ship,gameBoard,humanPlayer,ai} from './gameLogic';
+
+const friendlyAreaGameboard=document.querySelector('#friendly-area-gameboard');
+const enemyAreaGameboard=document.querySelector('#enemy-area-gameboard');
+
+function createGameBoardDom(gameBoardContainerName){
+    const gridSize=10;
+    const gridSquare=100;
+    gameBoardContainerName.style.gridTemplateRows=`repeat(${gridSize},1fr)`;
+    gameBoardContainerName.style.gridTemplateColumns=`repeat(${gridSize},1fr)`;
+    let squareDiv=[];
+    for (let i=0;i<gridSquare;i++){
+        squareDiv[i]=document.createElement('div');
+        squareDiv[i].classList.add('square_div');
+        gameBoardContainerName.appendChild(squareDiv[i]);
+    }
+}
+
+createGameBoardDom(friendlyAreaGameboard);
+createGameBoardDom(enemyAreaGameboard);
