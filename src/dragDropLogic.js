a95bc741a0047ec59d2312d27db1121adb3265d3
ridwan <irfanridwan20@gmail.com>
2022-05-27 11:36:17 +0600

    added eventListener to all draggable divs to know where in the div the mouse is on when the it is being dragged

--- src/dragDropLogic.js
+++ src/dragDropLogic.js
@@ -1,2 +1,13 @@
 import {humanPlayer} from'./gameLogic';
+function addDragDropFeature(){
+    const allDraggableDivs=document.querySelectorAll('.draggable');
+    allDraggableDivs.forEach(div=>{
+        for(let i=0;i<div.children.length;i++){
+            div.children[i].addEventListener('mousedown',(e)=>{
+                div.dataset.index=e.target.dataset.index; //this will give the position of draggable div on which mouse is on.
+            })
+        }
+    })
+}
 
+export {addDragDropFeature}
