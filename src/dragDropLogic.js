d7b4d3d0e481ef5c8a386075dca2ce4e92c03fcb
ridwan <irfanridwan20@gmail.com>
2022-05-28 17:59:14 +0600

    added checkIfDropValid function to check if a drop target is a valid drop target

--- src/dragDropLogic.js
+++ src/dragDropLogic.js
@@ -30,11 +30,35 @@ function addDragDropFeature(){
 
     }
     
+    function checkIfDropValid(event,shipData){
+        const dropTargetCoordinates=event.target.dataset.index.split(',');
+        const humanGameboardCellsArray=[...humanGameboardCells];
+        let cells_With_Same_Y_Axis_As_DropTarget=humanGameboardCellsArray.filter(cell=>{
+            const yAxisOfCell=cell.dataset.index.split(',')[1];
+            const yAxisOfDropTarget=dropTargetCoordinates[1];
+            return yAxisOfCell===yAxisOfDropTarget;
+        })
+        const xAxisOfFirstCell=cells_With_Same_Y_Axis_As_DropTarget[0].dataset.index.split(',')[0];
+        const xAxisOfLastCell=cells_With_Same_Y_Axis_As_DropTarget[cells_With_Same_Y_Axis_As_DropTarget.length-1].dataset.index.split(',')[0];
+        const positionOfMouseOnTheShip=shipData[0];
+        const xAxisOfDroppedShipFirstPosition=dropTargetCoordinates[0]-positionOfMouseOnTheShip;
+        const shiplength=Number(shipData[1]);
+        if(xAxisOfFirstCell<=xAxisOfDroppedShipFirstPosition 
+            && (xAxisOfLastCell>=xAxisOfDroppedShipFirstPosition+(shiplength-1))){
+    // shilplength-1 because 95+5=100 but if you consider 95 and add 5 to it then it would be 99
+    // you have to consider this nuance when working with gameboard cells
+            return true;
+        }else{return false}
+    }
+
     function drop(e){
         e.stopPropagation(); // stops the browser from redirecting.
         const xAxisOfDropTarget=Number(e.target.dataset.index.split(',')[0]);
         const shipDataJson=e.dataTransfer.getData('ship-data');
         const shipData=JSON.parse(shipDataJson);
+        if(!checkIfDropValid(e,shipData)){
+            return false; //this will stop the function and thus the drop will not be handled
+        };
         const shiplength=shipData[1];
         const positionOfMouseOnTheShip=shipData[0];
         for(let i=0;i<shiplength;i++){
