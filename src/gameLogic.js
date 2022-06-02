98bd498a45fea5125f70cf14da318c760f1646f2
hattima-tim <irfanridwan20@gmail.com>
2022-06-02 16:40:24 +0000

    prettier fixes

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -1,99 +1,118 @@
-function ship(shipname,coordinate){
-    let shipLength;
-    switch(shipname){
-        case 'carrier':
-            shipLength=5;
-            break;
-        case 'battleship':
-            shipLength=4;
-            break;
-        case ('destroyer'):
-            shipLength=3;
-            break;
-        case ('submarine'):
-            shipLength=2;
-            break;
+function ship(shipname, coordinate) {
+  let shipLength;
+  switch (shipname) {
+    case "carrier":
+      shipLength = 5;
+      break;
+    case "battleship":
+      shipLength = 4;
+      break;
+    case "destroyer":
+      shipLength = 3;
+      break;
+    case "submarine":
+      shipLength = 2;
+      break;
+  }
+  let hitPositions = [];
+  function hit(hitCoordinate) {
+    hitPositions.push(hitCoordinate);
+  }
+  function isSunk() {
+    if (hitPositions.length === shipLength) {
+      return true;
     }
-    let hitPositions=[];
-    function hit(hitCoordinate){
-        hitPositions.push(hitCoordinate);
-    }
-    function isSunk(){
-        if(hitPositions.length===shipLength){
-            return true;
-        }
-        return false;
-    }
-    return {coordinate,shipLength,hitPositions,hit,isSunk}
+    return false;
+  }
+  return { coordinate, shipLength, hitPositions, hit, isSunk };
 }
 
-function gameBoard(){
-    let shipList=[];
-    function placeShip(shipname,coordinate){
-        shipList.push(ship(shipname,coordinate));
+function gameBoard() {
+  let shipList = [];
+  function placeShip(shipname, coordinate) {
+    shipList.push(ship(shipname, coordinate));
+  }
+  let missedHits = [];
+  function receiveAttack(hitCoordinate) {
+    for (let i = 0; i < shipList.length; i++) {
+      if (
+        hitCoordinate >= shipList[i].coordinate &&
+        hitCoordinate < shipList[i].coordinate + shipList[i].shipLength
+      ) {
+        shipList[i].hit(hitCoordinate);
+        break;
+      } else if (i === shipList.length - 1) {
+        //means,we are at the end of the loop but we did not find a hit
+        missedHits.push(hitCoordinate);
+      }
     }
-    let missedHits=[];
-    function receiveAttack(hitCoordinate){
-        for(let i=0;i<shipList.length;i++){
-            if(hitCoordinate>=shipList[i].coordinate && hitCoordinate<(shipList[i].coordinate+shipList[i].shipLength)){
-                shipList[i].hit(hitCoordinate);
-                break;
-            }else if(i===shipList.length-1){ //means,we are at the end of the loop but we did not find a hit
-                missedHits.push(hitCoordinate)
-            }
-        }
-    }
-    return {shipList,placeShip,receiveAttack,missedHits} 
+  }
+  return { shipList, placeShip, receiveAttack, missedHits };
 }
 
-function humanPlayer(){
-    const gameboard=gameBoard();
-    function attack(enemyGameBoard,attackCoordinate){
-        enemyGameBoard.receiveAttack(attackCoordinate);
-    }
-    return {gameboard,attack}
+function humanPlayer() {
+  const gameboard = gameBoard();
+  function attack(enemyGameBoard, attackCoordinate) {
+    enemyGameBoard.receiveAttack(attackCoordinate);
+  }
+  return { gameboard, attack };
 }
 
 function calculateShotCoordinate(
-    previousEnemyGameBoardHitPositions,
-    enemyGameBoard,
-    coordinatesForAttack
-    ){
-    let updatedEnemyGameboardHitPositions = [];
-    enemyGameBoard.shipList.forEach((ship)=>{
-        updatedEnemyGameboardHitPositions=updatedEnemyGameboardHitPositions.concat(ship.hitPositions);
-    });
-    
-    const oldLength=previousEnemyGameBoardHitPositions.length;
-    const newLength=updatedEnemyGameboardHitPositions.length;
-    let shotCoordinate;
-    
-    if(oldLength<newLength){ //means last attack was a hit
-        previousEnemyGameBoardHitPositions.push(updatedEnemyGameboardHitPositions);
-        shotCoordinate = updatedEnemyGameboardHitPositions[newLength-1]+1;
-        coordinatesForAttack.splice(coordinatesForAttack.indexOf(shotCoordinate),1);
-        return shotCoordinate;
-    }else{
-        shotCoordinate = coordinatesForAttack[Math.floor(Math.random()*coordinatesForAttack.length)];
-        coordinatesForAttack.splice(coordinatesForAttack.indexOf(shotCoordinate),1);
-        return shotCoordinate;
-    }
+  previousEnemyGameBoardHitPositions,
+  enemyGameBoard,
+  coordinatesForAttack
+) {
+  let updatedEnemyGameboardHitPositions = [];
+  enemyGameBoard.shipList.forEach((ship) => {
+    updatedEnemyGameboardHitPositions =
+      updatedEnemyGameboardHitPositions.concat(ship.hitPositions);
+  });
+
+  const oldLength = previousEnemyGameBoardHitPositions.length;
+  const newLength = updatedEnemyGameboardHitPositions.length;
+  let shotCoordinate;
+
+  if (oldLength < newLength) {
+    //means last attack was a hit
+    previousEnemyGameBoardHitPositions.push(updatedEnemyGameboardHitPositions);
+    shotCoordinate = updatedEnemyGameboardHitPositions[newLength - 1] + 1;
+    coordinatesForAttack.splice(
+      coordinatesForAttack.indexOf(shotCoordinate),
+      1
+    );
+    return shotCoordinate;
+  } else {
+    shotCoordinate =
+      coordinatesForAttack[
+        Math.floor(Math.random() * coordinatesForAttack.length)
+      ];
+    coordinatesForAttack.splice(
+      coordinatesForAttack.indexOf(shotCoordinate),
+      1
+    );
+    return shotCoordinate;
+  }
 }
 
-function ai(){
-       const gameboard=gameBoard();
-       const gameBoardSize=100;
-       let coordinatesForAttack=[];
-       for(let i=0;i<gameBoardSize;i++){
-           coordinatesForAttack.push(i);
-       }
-       let previousEnemyGameBoardHitPositions=[];
-       
-       function attack(enemyGameBoard){           
-           const shotCoordinate=calculateShotCoordinate(previousEnemyGameBoardHitPositions,enemyGameBoard,coordinatesForAttack);
-           enemyGameBoard.receiveAttack(shotCoordinate);
-       }
-       return {gameboard,attack}
+function ai() {
+  const gameboard = gameBoard();
+  const gameBoardSize = 100;
+  let coordinatesForAttack = [];
+  for (let i = 0; i < gameBoardSize; i++) {
+    coordinatesForAttack.push(i);
+  }
+  let previousEnemyGameBoardHitPositions = [];
+
+  function attack(enemyGameBoard) {
+    const shotCoordinate = calculateShotCoordinate(
+      previousEnemyGameBoardHitPositions,
+      enemyGameBoard,
+      coordinatesForAttack
+    );
+    enemyGameBoard.receiveAttack(shotCoordinate);
+  }
+  return { gameboard, attack };
 }
 
-export {ship,gameBoard,humanPlayer,ai}
+export { ship, gameBoard, humanPlayer, ai };
--- src/index.js
+++ src/index.js
@@ -1,84 +1,91 @@
-import {humanPlayer,ai} from './gameLogic';
-import {addDragDropFeature} from './dragDropLogic';
+import { humanPlayer, ai } from "./gameLogic";
+import { addDragDropFeature } from "./dragDropLogic";
 
-const friendlyAreaGameboard=document.querySelector('#friendly-area-gameboard');
-const enemyAreaGameboard=document.querySelector('#enemy-area-gameboard');
+const friendlyAreaGameboard = document.querySelector(
+  "#friendly-area-gameboard"
+);
+const enemyAreaGameboard = document.querySelector("#enemy-area-gameboard");
 
-function createGameBoardDom(gameBoardContainerName){
-    const gridSize=10;
-    const gridSquare=100;
-    gameBoardContainerName.style.gridTemplateRows=`repeat(${gridSize},1fr)`;
-    gameBoardContainerName.style.gridTemplateColumns=`repeat(${gridSize},1fr)`;
-    let squareDiv=[];
-    let loopCount=1;
-    let yAxis=1;
-    for (let i=0;i<gridSquare;i++){
-        squareDiv[i]=document.createElement('div');
-        squareDiv[i].setAttribute('data-index',`${[i,yAxis]}`);
-        if(loopCount===10){
-            yAxis+=1;
-            loopCount=1;
-        }else{
-            loopCount+=1;
-        }
-        squareDiv[i].classList.add('square_div');
-        gameBoardContainerName.appendChild(squareDiv[i]);
+function createGameBoardDom(gameBoardContainerName) {
+  const gridSize = 10;
+  const gridSquare = 100;
+  gameBoardContainerName.style.gridTemplateRows = `repeat(${gridSize},1fr)`;
+  gameBoardContainerName.style.gridTemplateColumns = `repeat(${gridSize},1fr)`;
+  let squareDiv = [];
+  let loopCount = 1;
+  let yAxis = 1;
+  for (let i = 0; i < gridSquare; i++) {
+    squareDiv[i] = document.createElement("div");
+    squareDiv[i].setAttribute("data-index", `${[i, yAxis]}`);
+    if (loopCount === 10) {
+      yAxis += 1;
+      loopCount = 1;
+    } else {
+      loopCount += 1;
     }
+    squareDiv[i].classList.add("square_div");
+    gameBoardContainerName.appendChild(squareDiv[i]);
+  }
 }
 
 createGameBoardDom(friendlyAreaGameboard);
 createGameBoardDom(enemyAreaGameboard);
 
-const human=humanPlayer();
-addDragDropFeature(human)
+const human = humanPlayer();
+addDragDropFeature(human);
 //human goes into this function and get changed
 //but since human is an object we will get an updated human object in this module
 
-function markShipsInTheDom(humanGameBoard){
-    humanGameBoard.shipList.forEach(ship => {
-        for(let i=0;i<ship.shipLength;i++){
-            friendlyAreaGameboard.children[ship.coordinate+i].style.background='#444444';
-        }
+function markShipsInTheDom(humanGameBoard) {
+  humanGameBoard.shipList.forEach((ship) => {
+    for (let i = 0; i < ship.shipLength; i++) {
+      friendlyAreaGameboard.children[ship.coordinate + i].style.background =
+        "#444444";
+    }
+  });
+}
+
+function markHitUnhit(enemy, enemyGameboardDom) {
+  enemy.gameboard.shipList.forEach((ship) => {
+    ship.hitPositions.forEach((position) => {
+      enemyGameboardDom.children[position].textContent = "x";
     });
+  });
+  enemy.gameboard.missedHits.forEach((missedHitPosition) => {
+    enemyGameboardDom.children[missedHitPosition].textContent = "o";
+  });
 }
 
-function markHitUnhit(enemy,enemyGameboardDom){
-    enemy.gameboard.shipList.forEach((ship)=>{
-        ship.hitPositions.forEach(position=>{
-            enemyGameboardDom.children[position].textContent='x';
-        })
-    })
-    enemy.gameboard.missedHits.forEach(missedHitPosition=>{
-        enemyGameboardDom.children[missedHitPosition].textContent='o';
-    })
+function itIsAiTurn(ai, human) {
+  ai.attack(human.gameboard);
+  markHitUnhit(human, friendlyAreaGameboard);
 }
 
-function itIsAiTurn(ai,human){
-    ai.attack(human.gameboard);
-    markHitUnhit(human,friendlyAreaGameboard)
+function addEventListenerToAiGameBoard(aiPlayer, human) {
+  enemyAreaGameboard.childNodes.forEach((child) => {
+    child.addEventListener(
+      "click",
+      (e) => {
+        const targetIndex = parseInt(e.target.dataset.index.split(",")[0]);
+        aiPlayer.gameboard.receiveAttack(targetIndex);
+        markHitUnhit(aiPlayer, enemyAreaGameboard);
+        itIsAiTurn(aiPlayer, human);
+      },
+      { once: true }
+    );
+  });
 }
 
-function addEventListenerToAiGameBoard(aiPlayer,human){
-    enemyAreaGameboard.childNodes.forEach((child)=>{
-        child.addEventListener('click',(e)=>{
-            const targetIndex=parseInt(e.target.dataset.index.split(',')[0]);
-            aiPlayer.gameboard.receiveAttack(targetIndex);
-            markHitUnhit(aiPlayer,enemyAreaGameboard);
-            itIsAiTurn(aiPlayer,human)
-        }, {once : true})
-    })  
+function playGame() {
+  const computer = ai();
+  computer.gameboard.placeShip("carrier", 4);
+  computer.gameboard.placeShip("battleship", 14);
+  computer.gameboard.placeShip("destroyer", 34);
+  computer.gameboard.placeShip("submarine", 54);
+  addEventListenerToAiGameBoard(computer, human);
 }
 
-function playGame(){
-    const computer=ai();
-    computer.gameboard.placeShip('carrier',4);
-    computer.gameboard.placeShip('battleship',14);
-    computer.gameboard.placeShip('destroyer',34);
-    computer.gameboard.placeShip('submarine',54);
-    addEventListenerToAiGameBoard(computer,human);
-}
-
-const startGameButton=document.querySelector('#start');
-startGameButton.addEventListener('click',()=>{
-    playGame();
-})
+const startGameButton = document.querySelector("#start");
+startGameButton.addEventListener("click", () => {
+  playGame();
+});
